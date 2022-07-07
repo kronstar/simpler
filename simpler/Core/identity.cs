87af216c570e50e7577656947501566f7b1cc041
kronstar <baduy.work@gmail.com>
2022-07-07 20:38:02 +0700

    Add project files.

---
+++ simpler/Core/identity.cs
@@ -0,0 +1,35 @@
+﻿namespace simpler.Core
+{
+	public static class Identity
+	{
+		/// <summary>
+		/// Return unique user id
+		/// </summary>
+		/// <returns>User id as a unique string</returns>
+		public static string CreateUUID()
+		{
+			Guid myuuid = Guid.NewGuid();
+			return myuuid.ToString();
+		}
+
+		/// <summary>
+		/// Return product id
+		/// </summary>
+		/// <param name="value">Product name to generate id</param>
+		/// <returns>Product id as a unique string</returns>
+		public static string CreateProductID(string value)
+		{
+			return value;
+		}
+		/// <summary>
+		/// Return Order id
+		/// </summary>
+		/// <param name="value">Order id returned</param>
+		/// <returns>Order id as a unique string</returns>
+		public static string CreateOrderID(string value)
+		{
+			string current_date = DateTime.Now.Ticks.ToString();
+			return value;
+		}
+	}
+}
---
+++ simpler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace simpler.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
